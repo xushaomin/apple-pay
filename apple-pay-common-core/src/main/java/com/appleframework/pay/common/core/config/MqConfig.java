071dc7607e538a8426acea1ba5d02d636edd9abe
xushaomin <xushaomin@foxmail.com>
2017-05-23 17:06:00 +0800

    增加支付宝、微信查询接口

---
+++ apple-pay-common-core/src/main/java/com/appleframework/pay/common/core/config/MqConfig.java
@@ -0,0 +1,11 @@
+package com.appleframework.pay.common.core.config;
+
+public class MqConfig {
+		
+	/** 商户通知队列 **/
+	public static String MERCHANT_NOTIFY_QUEUE = "mq.queue.notify";
+
+	/** 订单通知队列 **/
+	public static String ORDER_NOTIFY_QUEUE    = "mq.queue.query";
+    
+}
